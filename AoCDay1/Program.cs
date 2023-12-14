41deed0e8bd83414a4356ace23144db027f5bc85
finneaa <finneaa@student.matnat.uio.no>
2023-12-14 14:53:49 +0100

    Added comments and Start of part2 function

--- AoCDay1/Program.cs
+++ AoCDay1/Program.cs
@@ -2,6 +2,13 @@
 using System.IO;
 using System.Collections;
 
+/// <summary>
+/// FileReader is a class with one function. The function takes in an string containing the location of the file to be read. 
+/// The function returns an String array with each read line.
+/// For my own conveience the path i using is just put into class Programs Main.
+/// This will ned to be change for testing other files or for other people trying the program.
+/// The function ReadFile reads the entire file and than split each line into an seperate string in the array.
+/// </summary>
 public class FileReader
 {
     public string[] ReadFile(String path)
@@ -13,9 +20,10 @@ public class FileReader
             // Open the text file using a stream reader.
             using (var sr = new StreamReader(path))
             {
-                // Read the stream as a string, and write the string to the console.
+                // Read the stream as a string.
                 String full_Text = "";
                 full_Text =sr.ReadToEnd();
+                //Split String
                 fileInput = full_Text.Split("\r\n");
             }
         }
@@ -29,7 +37,41 @@ public class FileReader
 }
 class Program
 {
+    /// <summary>
+    /// Function that takes in a string and finds all numbers in it, than combines the first found number and last found number into a 2 diget number. 
+    /// returns the value of the 2 diget number.
+    /// </summary>
+    /// <param name="line"><String to look for numbers in>
+    /// <returns><2 diget number made by the first and the last found number i line>
       public int FindNumber(String line)
+    {
+        int number = 0;
+        List<string> foundNumbers = new List<string>();
+        foreach (char c in line)
+        {
+            if(char.IsDigit(c)){
+                foundNumbers.Add(Char.ToString(c));
+                
+            }
+        }
+        String tempNumber = "Failed";
+        if(foundNumbers.Count>=1){
+            tempNumber = string.Concat(foundNumbers[0], foundNumbers[foundNumbers.Count-1]);
+            Int32.TryParse(tempNumber, out number);
+            //Console.WriteLine(tempNumber);
+        }
+        return number;
+
+    }
+
+    /// <summary>
+    /// Function that takes in a string and finds all numbers in it( now also with spelled numbers like "One"), 
+    /// than combines the first found number and last found number into a 2 diget number. 
+    /// returns the value of the 2 diget number.
+    /// </summary>
+    /// <param name="line"><String to look for numbers in>
+    /// <returns><2 diget number made by the first and the last found number i line>
+      public int FindNumberAdvansed(String line)
     {
         int number = 0;
         List<string> foundNumbers = new List<string>();
